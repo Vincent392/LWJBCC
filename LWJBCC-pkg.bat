47f8eaa7e8ab41f3ebdea5699ecb23b814e3a998
Vincent392 <90470156+Vincent392@users.noreply.github.com>
2023-12-18 16:01:59 +0000

    updates

--- LWJBCC-pkg.bat
+++ LWJBCC-pkg.bat
@@ -1,20 +1,26 @@
 @Echo off
 TITLE LWJBCC
-:nopkg
+:pkg
 cls
 Echo LWJBCC
-Echo Lightweight Java Batch Code Compiler
+Echo Lightweight Java Batch Code Compiler PKG
 Echo Version 1.0.1 (CODENAME: packages work!)
 echo ================================================
 echo jar file name (does not support spaces)
-set /p cmd=">"
+set /p cmd00=">"
 echo main class name (does not support spaces)
-set /p cmd1=">"
+set /p cmd11=">"
+echo package start location
+set /p cmd12=">"
+echo package mid location
+set /p cmd13=">"
+echo package end location
+set /p cmd14=">"
 echo ================================================
 echo compiling .java files... (THIS CAN TAKE A WHILE)
-javac ./%cmd2%/*.java
+javac ./%cmd12%/%cmd13%/%cmd14%/*.java
 echo making .jar... (THIS CAN TAKE A WHILE)
-jar cvfe %cmd%.jar %cmd1% *.class
+jar cvfe %cmd00%.jar %cmd12%.%cmd13%.%cmd14%.%cmd11% ./%cmd12%/%cmd13%/%cmd14%/*.class
 echo jar made.
 PAUSE
 echo testing jar...
